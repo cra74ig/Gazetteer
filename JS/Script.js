8c3dfb15ab10ef8ccb4f88477d5e888a9d07125f
Craig Button <ctbutton@sky.com>
2020-12-06 14:44:45 +0000

    Complete? 06/12/2020

--- JS/Script.js
+++ JS/Script.js
@@ -29,27 +29,40 @@ var n = d.getDay()
 switch (n){
     case n=0:
         var day3= "Tuesday";
+        var day4= "Wednesday";
+        var day5= "Thursday";
         break;
     case n=1:
         var day3= "Wednesday";
+        var day4= "Thursday";
+        var day5= "Friday";
         break;
     case n=2:
         var day3= "Thursday";
+        var day4= "Friday";
+        var day5= "Saturday";
         break;
     case n=3:
         var day3= "Friday";
+        var day4= "Saturday";
         break;
     case n=4:
         var day3= "Saturday";
+        var day4= "Sunday";
+        var day5= "Monday";
         break;   
     case n=5:
         var day3= "Sunday";
+        var day4= "Monday";
+        var day5= "Tuesday";
         break; 
     case n=6:
         var day3= "Monday";
+        var day4= "Tuesday";
+        var day5= "Wednesday";
         break; 
 }
-var markerGroup = L.layerGroup().addTo(mymap);
+
 
 L.Control.select = L.Control.extend({
     onAdd: function(map) {
@@ -76,7 +89,7 @@ L.Control.data = L.Control.extend({
     onAdd: function(map) {
         var form = L.DomUtil.create('div');
         form.id = "DataTable";
-        form.innerHTML = "<div class='container'><ul class='nav nav-pills'><li class='active'><a href='#Overview' data-toggle='tab'>Overview</a></li><li><a href='#picture' data-toggle='tab'>Picture</a></li><li><a href='#weather' data-toggle='tab'>Weather</a></li><li><a href='#currency-tab' data-toggle='tab'>Currency</a></li></ul><div class='tab-content clearfix'> <div class='tab-pane active' id='Overview'><table class='table table-light table-bordered'><tr><th>Country</th><td id='countryName'></td></tr><tr><th>Capital City</th><td id='capitalCity'></td></tr><tr><th>Population</th><td id='Population'></td></tr><tr><th>Wikipedia Tourist Links</th><td id='WikiLinks'></td></tr></table> </div> <div class='tab-pane' id='picture'> </div> <div class='tab-pane' id='weather'><table class='table table-light table-bordered'><thead><th>Current</th><th>Tomorrow</th><th>"+day3+"</th></thead><tbody><tr><td id='currentWeather'></td><td id='weatherDay2'></td><td id='weatherDay3'></td></tr></tbody></table></div> <div class='tab-pane' id='currency-tab'><table class='table table-light table-bordered'><thead><th>Currency</th><th>GBP</th><th>USD</th><th>EUR</th></thead><tbody><tr><td id='currency'></td><td id='GBP'></td><td id='USD'></td><td id='EUR'></td></tr></tbody></table></div></div></div>";
+        form.innerHTML = "<div class='container'><ul class='nav nav-pills'><li class='active'><a href='#Overview' data-toggle='tab'>Overview</a></li><li><a href='#picture' data-toggle='tab'>Picture</a></li><li><a href='#weather' data-toggle='tab'>Weather</a></li><li><a href='#currency-tab' data-toggle='tab'>Currency</a></li></ul><div class='tab-content clearfix'> <div class='tab-pane active' id='Overview'><table class='table table-light table-bordered'><tr><th>Country</th><td id='countryName'></td></tr><tr><th>Capital City</th><td id='capitalCity'></td></tr><tr><th>Population</th><td id='Population'></td></tr><tr><th>Wikipedia Tourist Links</th><td id='WikiLinks'></td></tr></table> </div> <div class='tab-pane' id='picture'> <div class='container'><div id='myCarousel' class='carousel slide' data-ride='carousel'><ol class='carousel-indicators'><li data-target='#myCarousel' data-slide-to='0' class='active'></li><li data-target='#myCarousel' data-slide-to='1'></li><li data-target='#myCarousel' data-slide-to='2'></li></ol><div class='carousel-inner' id='Pictures'></div><a class='left carousel-control' href='#myCarousel' data-slide='prev'><span class='glyphicon glyphicon-chevron-left'></span><span class='sr-only'>Previous</span></a><a class='right carousel-control' href='#myCarousel' data-slide='next'><span class='glyphicon glyphicon-chevron-right'></span><span class='sr-only'>Next</span></a></div></div></div> <div class='tab-pane' id='weather'><table class='table table-light table-bordered'><thead><th>Current</th><th>Tomorrow</th><th>"+day3+"</th><th>"+day4+"</th></thead><tbody><tr><td id='currentWeather'></td><td id='weatherDay2'></td><td id='weatherDay3'></td><td id='weatherDay4'></td></tr></tbody></table></div> <div class='tab-pane' id='currency-tab'><table class='table table-light table-bordered'><thead><th>Currency</th><th>GBP</th><th>USD</th><th>EUR</th></thead><tbody><tr><td id='currency'></td><td id='GBP'></td><td id='USD'></td><td id='EUR'></td></tr></tbody></table></div></div></div>";
         return form;
     },
 
@@ -187,19 +200,34 @@ function main(countryCode){
                                 });
                         });
                     };
-                    
-                
-                var polygon = L.polygon(coordsArray,{color: 'blue'}).bindTooltip(result.data["geonames"][0]["countryName"],{
+                if (mymap.hasLayer(window.markerGroup)) {
+                    console.log("test");
+                    mymap.removeLayer(window.markerGroup);
+                    }
+                window.markerGroup = L.layerGroup().addTo(mymap);
+                window.polygon = L.polygon(coordsArray,{color: 'blue'}).bindTooltip(result.data["geonames"][0]["countryName"],{
                     sticky: true
-                }).addTo(mymap);
-               
+                }).addTo(markerGroup);
+                var pictureHtml; 
+                var first = true
+                result.data["webPicture"].forEach(picture => {
+                    if (first){
+                        pictureHtml="<div class='item active'><img src='"+picture+"'></div>"
+                        first= false;
+                        console.log(pictureHtml)
+                    }else{
+                        pictureHtml = pictureHtml + "<div class='item'><img src='"+picture+"'></div>"
+                        console.log(pictureHtml)
+                    }
+                });
                 // zoom the map to the polyline
                 mymap.fitBounds(polygon.getBounds());
                 $("#News").html("<h2>Global News</h2><h3>"+result.data["news"]["title"]+"</h3>"+result.data["news"]["description"]+"<a href="+result.data["news"]["url"]+">Read more</a>")
-                $('#picture').html("<img class='webPicture' src='"+result.data["webPicture"] + "'>");
+                $('#Pictures').html(pictureHtml);
                 $('#currentWeather').html("<img class='weather' src='"+result.data["Weather"]["current"]["condition"]["icon"] + "'><h3>"+result.data["Weather"]["current"]["temp_c"]+"&#8451<h3>");
                 $('#weatherDay2').html("<img class='weather' src='"+result.data["Weather"]["day2"]["condition"]["icon"] + "'><h3>"+result.data["Weather"]["day2"]["maxtemp_c"]+"&#8451</h3><p>"+result.data["Weather"]["day2"]["mintemp_c"]+"&#8451</p>");
                 $('#weatherDay3').html("<img class='weather' src='"+result.data["Weather"]["day3"]["condition"]["icon"] + "'><h3>"+result.data["Weather"]["day3"]["maxtemp_c"]+"&#8451</h3><p>"+result.data["Weather"]["day3"]["mintemp_c"]+"&#8451</p>");
+                $('#weatherDay4').html("<img class='weather' src='"+result.data["Weather"]["day4"]["condition"]["icon"] + "'><h3>"+result.data["Weather"]["day4"]["maxtemp_c"]+"&#8451</h3><p>"+result.data["Weather"]["day4"]["mintemp_c"]+"&#8451</p>");
                 $('#countryName').html(result.data["geonames"][0]["countryName"]);
                 $('#capitalCity').html(result.data["geonames"][0]["capital"]);
                 $('#Population').html(result.data["geonames"][0]["population"]);
@@ -239,7 +267,7 @@ function GetCountryCode(position) {
         icon: CurrentIcon
       }).bindTooltip("Current Location", {
           sticky: true
-      }).addTo(markerGroup);
+      }).addTo(mymap);
     $.ajax({
         url: "PHP/GetCountry.PHP",
         type: 'POST',
